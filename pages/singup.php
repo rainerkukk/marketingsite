2e64f66272026ad9a8e264ec619839f8d63f3c6f
quruaj <maria-helena.preiman@khk.ee>
2013-02-07 15:31:02 +0200

    commit

---
+++ pages/singup.php
@@ -0,0 +1,7 @@
+<form>
+	<method="post" action="?page=reg"><br>
+	eesnimi: <input type="text" name="fname"><br>
+	perenimi: <input type="text" name="lname"><br>
+	e-mail: <input type="text" name="email"><br>
+<input type="submit" value="Esita" />
+</form>
